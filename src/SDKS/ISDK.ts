221bb9795b684a721592a711faef9d3cc2cd0b47
James Kirby <nshcore@protonmail.com>
2018-11-16 13:54:48 +0000

    add interface and abstract wallet impl

--- src/SDKS/ISDK.ts
+++ src/SDKS/ISDK.ts
@@ -1,5 +1,27 @@
+import * as IWIF from './IWIF';
+
 export namespace CryptyoWallet.SDKS {
   export interface ISDK {
+
+    generateKeyPair(entropy : string) : Object;
+
+    importWIF(wif : IWIF.CryptyoWallet.SDKS.IWIF) : Object;
+
+    gernerateP2SHMultiSig(key1 : string, key2 : string, key3 : string) : Object;
+
+    generateSegWitAddress() : Object;
+
+    generateSegWitP2SH() : Object;
+
+    generateSegWit3of4MultiSigaddress(key1 : string, key2 : string, key3 : string) : Object;
+
+    generateTestNetAddress() : Object;
+
+    create1t1tx() : Object;
+
+    create2t2tx() : Object;
+
+    verifyTxSignature(signature : string) : boolean;
 
   }
 }
