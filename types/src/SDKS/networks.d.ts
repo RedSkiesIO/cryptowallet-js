2693e36b7fb2bfea779358b232797cc3c4c351b3
konradmoskal <konrad.moskal@atlascity.io>
2018-11-27 16:45:11 +0000

    update

--- types/src/SDKS/networks.d.ts
+++ types/src/SDKS/networks.d.ts
@@ -10,22 +10,6 @@ export declare const BITCOIN: {
     scriptHash: number;
     wif: number;
 };
-<<<<<<< HEAD
-export declare const BITCOIN_TESTNET: {
-    name: string;
-    messagePrefix: string;
-    bech32: string;
-    bip32: {
-        public: number;
-        private: number;
-    };
-    bip: number;
-    pubKeyHash: number;
-    scriptHash: number;
-    wif: number;
-};
-=======
->>>>>>> 9fe4613d62133d4d7493e835b46567bdb0a085aa
 export declare const LITECOIN: {
     name: string;
     messagePrefix: string;
@@ -76,8 +60,6 @@ export declare const ETHEREUM: {
     name: string;
     bip: number;
 };
-<<<<<<< HEAD
-=======
 export declare const testnet: {
     messagePrefix: string;
     bech32: string;
@@ -114,4 +96,3 @@ export declare const BITCOIN_TESTNET: {
         wif: number;
     };
 };
->>>>>>> 9fe4613d62133d4d7493e835b46567bdb0a085aa
