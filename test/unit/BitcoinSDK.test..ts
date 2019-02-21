57b73715bf0437abdca57114aa33093ec24b2355
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-02-21 16:17:26 +0000

    removed unused import

--- test/unit/BitcoinSDK.test..ts
+++ test/unit/BitcoinSDK.test..ts
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-unresolved */
 import 'jest';
-import { KeyPair } from 'src/SDKS/GenericSDK.d';
 import axios from 'axios';
 import { CryptoWallet } from '../../src/SDKFactory';
 
