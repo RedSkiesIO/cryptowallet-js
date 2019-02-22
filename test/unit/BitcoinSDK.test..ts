9a866da6c7ffd31e4e7a887c5d01903c4692605d
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-02-22 10:43:56 +0000

    added unit tests for generateSegWitP2SH

--- test/unit/BitcoinSDK.test..ts
+++ test/unit/BitcoinSDK.test..ts
@@ -28,10 +28,15 @@ describe('bitcoinSDK (wallet)', () => {
 
   describe('generateSegWitP2SH', () => {
     it('can generate a BTC testnet segwit address', () => {
-      const wallet: any = btc.generateHDWallet(entropy, 'BITCOIN_TESTNET');
+      const wallet: any = btc.generateHDWallet(entropy, network);
       const keypair: any = btc.generateKeyPair(wallet, 0);
       const address = btc.generateSegWitP2SH(keypair);
       expect(address).toBe('2MyFPraHtEy2uKttPeku1wzokVeyJGTYvkf');
+    });
+    it('can detect if an invalid keypair is used', () => {
+      const wallet: any = eth.generateHDWallet(entropy, 'ETHEREUM');
+      const keypair: any = eth.generateKeyPair(wallet, 0);
+      expect(() => btc.generateSegWitAddress(keypair)).toThrow('Invalid keypair type');
     });
   });
 });
