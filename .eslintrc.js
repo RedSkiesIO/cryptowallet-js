3bdf944271044f2e49cf86a9f7df678c19dae1af
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-03-05 17:12:04 +0000

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   plugins: [
-    'typescript',
+    'typescript'
   ],
   extends: [
     'airbnb-base'
@@ -25,7 +25,7 @@ module.exports = {
     'import/no-unresolved': 'off',
     'spaced-comment': 'off',
     align: {
-      'mode': 'strict',
+      'mode': 'strict'
     }
   },
 }
