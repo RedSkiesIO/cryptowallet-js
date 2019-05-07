8f870ffd098b6d22cecb46f0280edc228d56312b
Stephen <stephen.horsfall@atlascity.io>
2019-05-07 14:29:51 +0100

    Fix vulnerable dependencies (#66)
    * fix dependencies
    
    * change lib to es5

--- src/SDKS/GenericSDK.ts
+++ src/SDKS/GenericSDK.ts
@@ -479,7 +479,7 @@ export namespace CryptoWallet.SDKS {
       if (!wallet || !wallet.network || !wallet.network.connect) {
         throw new Error('Invalid wallet type');
       }
-      const apiUrl: string = this.networks[wallet.network.name];
+      const apiUrl: string = this.networks[wallet.network.name].discovery;
       let usedAddresses: object[] = [];
       const usedAddressesIndex: number[] = [];
       let emptyAddresses: number[] = [];
